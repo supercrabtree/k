b62f98c5b37fefb8d6cf5e22bdc59278192e513f
George Crabtree <supercrabtree@googlemail.com>
2016-07-27 09:28:50 +0200

    Add filetypes test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 import test from 'ava';
 import {default as k, split} from './helpers/run-k-in-javascript';
 
-test('First proof of concept test', async t => {
+test('File and directory types', async t => {
   const lines = await k.stripColors('fixtures/one').then(split);
-  t.is(lines[0], 'total 16');
-  console.log(lines);
+  t.is(lines[0],  'total 16');
+  t.is(lines[1],  '-rwxr-sr-x 1 supercrabtree staff  0 10 Feb   20:19 | exe-with-gid');
+  t.is(lines[2],  '-rwsr-sr-x 1 supercrabtree staff  0 10 Feb   20:42 | exe-with-gid-and-uid');
+  t.is(lines[3],  '-rwsr-xr-x 1 supercrabtree staff  0 10 Feb   20:05 | exe-with-uid');
+  t.is(lines[4],  '-rwxr-xr-x 1 supercrabtree staff  0 24 Jan    2016 | executable');
+  t.is(lines[5],  'drwxr-xr-x 2 supercrabtree staff 68 24 Jan    2016 | executable-dir');
+  t.is(lines[6],  'lrwxr-xr-x 1 supercrabtree staff 10 24 Jan    2016 | link.txt -> string.txt');
+  t.is(lines[7],  'drwxr-xr-x 2 supercrabtree staff 68 24 Jan    2016 | nested-dir');
+  t.is(lines[8],  '-rw-r--r-- 1 supercrabtree staff 12 24 Jan    2016 | string.txt');
+  t.is(lines[9],  'drwxrwxrwt 2 supercrabtree staff 68 10 Feb   20:50 | wto-with-sticky');
+  t.is(lines[10], 'drwxrwxrwx 2 supercrabtree staff 68 10 Feb   20:50 | wto-without-sticky ');
 });
