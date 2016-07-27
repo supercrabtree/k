b045399c49c3b905fa6c4d8e5b27b7cd06bddee3
George Crabtree <supercrabtree@googlemail.com>
2016-07-27 09:07:15 +0200

    Add ability to pass any arguments to k in js

--- test/helpers/run-k-in-javascript.js
+++ test/helpers/run-k-in-javascript.js
@@ -5,13 +5,21 @@ const stripAnsi = require('strip-ansi');
 const kcommand = readFileSync('../k.sh', 'utf-8');
 
 function k() {
-  return execa('eval', [kcommand, 'k'], {shell: '/bin/zsh'})
+
+  const args = [kcommand, 'k'];
+
+  for (var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+
+  return execa('eval', args, {shell: '/bin/zsh'})
     .then(({stdout}) => stdout)
     .then(stdout => stdout.split('\n'));
 }
 
 k.stripColors = function () {
-  return k().then(lines => lines.map(line => stripAnsi(line)));
+  return k.apply(undefined, arguments)
+    .then(lines => lines.map(line => stripAnsi(line)));
 }
 
 module.exports = k;
