12c0ee091e301c540b1a39d0e9eae65734705eac
George Crabtree <supercrabtree@googlemail.com>
2016-07-27 07:59:24 +0200

    Rewrite async style

--- test/test.js
+++ test/test.js
@@ -1,23 +1,21 @@
 import test from 'ava';
-import {default as execa, spawn} from 'execa';
+import execa from 'execa';
 import {readFileSync} from 'fs';
 import stripAnsi from 'strip-ansi';
 
 const kcommand = readFileSync('../k.sh', 'utf-8');
 
-function k(path='') {
-  return execa('eval', [kcommand + 'k ' + path], {shell: '/bin/zsh'})
-    .then(({stdout}) => stdout)
-    .then(res => res.split('\n'));
+async function k(path='') {
+  const {stdout} = await execa('eval', [kcommand + 'k ' + path], {shell: '/bin/zsh'})
+  return stdout.split('\n');
 }
 
-k.stripColors = function (path) {
-  return k(path).then(res => res.map(line => stripAnsi(line)));
+k.stripColors = async function (path) {
+  const lines = await k(path);
+  return lines.map(line => stripAnsi(line));
 }
 
-test('First proof of concept test', t => {
-  return k.stripColors()
-    .then(lines => {
-      t.is(lines[0], 'total 8');
-    });
+test('First proof of concept test', async t => {
+  const lines = await k.stripColors();
+  t.is(lines[0], 'total 8');
 });
