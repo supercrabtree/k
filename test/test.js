ac964cac80e75169ca4cb557835f547467548004
George Crabtree <supercrabtree@googlemail.com>
2016-07-27 09:19:18 +0200

    Tidy fixtures folder

--- test/test.js
+++ test/test.js
@@ -4,4 +4,5 @@ import {default as k, split} from './helpers/run-k-in-javascript';
 test('First proof of concept test', async t => {
   const lines = await k.stripColors('fixtures/one').then(split);
   t.is(lines[0], 'total 16');
+  console.log(lines);
 });
